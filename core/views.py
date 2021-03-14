a74b9ab5763d8853c678d88d276e1a88518d399c
sauravfouzdar <sauravfouzdar@gmail.com>
2021-03-14 10:09:39 +0530

    Profile, Home page rendered with Post,followers,chat

--- core/views.py
+++ core/views.py
@@ -1,11 +1,21 @@
 from __future__ import unicode_literals
 from django.shortcuts import render, redirect
+from itertools import chain
+from subpages.models import memes, confessions, ml, WebDevelopment, examCorner
 
 # Create your views here.
 def home(request):
-    template =  'index.html' 
-    return render(request, template)
+    template_name =  'index.html' 
+    object_list = sorted(chain(
+        memes.objects.all(),
+        confessions.objects.all(),
+        ml.objects.all()
+    ), key=lambda obj: obj.created_on)
+
+    return render(request, template_name, {'feed': object_list})
 
 def profile(request):
+    obj = memes.objects.all().order_by('-id')
     template_name =  'user_profile.html' 
-    return render(request, template_name)    
+    context = {'obj':obj}
+    return render(request, template_name, context)    
